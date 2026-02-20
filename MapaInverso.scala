a5491f7b314a5e9ce9d2be66f727ee81e77631cf
Rodrigo Blanco (Alfa Formación) <rblanco.docente@alfa-formacion.com>
2026-02-20 13:34:51 +0100

    Añadir diccionario inverso

---
+++ MapaInverso.scala
@@ -0,0 +1,56 @@
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{Map => MutableMap}
+
+object MapaInverso {
+
+    def main(args: Array[String]): Unit = {
+
+        val edades = Map("Ana"-> 25, "Jose" -> 30, "Julio" -> 25)
+
+        val inverso = crearInverso(edades)
+
+        println("{")
+        inverso.foreach{ case (clave, valor) => println(s"$clave -> $valor")}
+        println("}")
+      
+
+        //Forma declarativa
+        val inverso1 = edades.map{ case(clave, valor) => (valor, clave) } //Ignoro repetidos
+
+        println("{")
+        inverso1.foreach{ case (clave, valor) => println(s"$clave -> $valor")}
+        println("}")
+
+    val inverso2 = edades
+    .groupBy{case (nombre, edad) => edad}
+    .view //Crear una evaluación perezosa
+    .mapValues(_.keys.toList)
+    .toMap //Deshacer la evaluación perezosa
+
+    // val inverso2 = edades.groupBy(_._2).mapValues(_.keys.toList) (alternativa más directa sin evaluación perezosa)
+
+    println("{")
+        inverso2.foreach{ case (clave, valor) => println(s"$clave -> $valor")}
+        println("}")
+
+    }
+
+
+    def crearInverso(mapa: Map[String, Int]): MutableMap[Int, ArrayBuffer[String]] = {
+
+        var inverso = MutableMap[Int, ArrayBuffer[String]]()
+
+        for ((nombre, edad) <- mapa) {
+
+            if (inverso.contains(edad)) inverso(edad).addOne(nombre)
+            else inverso += (edad, ArrayBuffer(nombre))
+
+        }
+
+        inverso
+
+    }
+
+
+
+}
